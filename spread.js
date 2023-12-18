822e13a9fa41721b6d0cb880e84c2fa809cc3612
sasiie <Sarchiradic99@gmail.com>
2023-12-18 12:48:12 +0100

    yes

--- spread.js
+++ spread.js
@@ -12,7 +12,7 @@ const person = {
 
   function uppdatePerson(person, adress){
     return {...person, adress:{
-        ...person.adress,...newAdress}
+        ...person.adress,...newAdress},{...}
     }
   };
   const newAdress = {
